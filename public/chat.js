a538a1b9893b1acf2217ec0ced0816663b602b85
Michael Efemena <mefemena@gmail.com>
2021-04-28 12:32:08 +0100

    add changeTab function

--- public/chat.js
+++ public/chat.js
@@ -1,17 +1,24 @@
 $(function () {
   var socket = io.connect("http://localhost:9000");
 
+  let activeUsers = [];
+
+  let selectedUser = null
+
   socket.on("users", ({ users }) => {
     console.log(users);
     updateUserList(users);
+    activeUsers.push(users);
   });
+
+  
 
   function updateUserList(users) {
     $("#active-users").html("");
     users.forEach((user) => {
       if (socket.id === user.id) {
         $("#active-users").append(
-          $(`<div class="chat_list active_chat">`).html(`
+          $(`<div class="chat_list active_chat" id="${user.id}">`).html(`
           <div class="chat_people">
                   <div class="chat_img">
                     <img
@@ -22,13 +29,20 @@ $(function () {
                   <div class="chat_ib">
                     <h5>Yourself <span class="chat_date">(yourself)</span></h5>
                   </div>
-                </div>
+            </div>
           `)
         );
+
+        $(`#${user.id}`).click(() => {
+          console.log("This is yourself");
+        });
         return;
       }
       $("#active-users").append(
-        $(`<div class="chat_list">`).html(
+        $(
+          `<div class="chat_list" id="${user.id}">
+          `
+        ).html(
           `
           <div class="chat_people">
                   <div class="chat_img">
@@ -42,10 +56,24 @@ $(function () {
                    
                   </div>
                 </div>
-                
           `
         )
       );
+      $(`#${user.id}`).click(() => {
+        console.log(user);
+      });
     });
+  }
+
+  function createUserMessageBox() {
+
+  }
+
+  function changeTab(id) {
+    selectedUser = id
+  }
+
+  function sendMessage(to, message) {
+      
   }
 });
